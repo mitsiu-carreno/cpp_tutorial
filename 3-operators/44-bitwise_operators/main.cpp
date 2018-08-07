b29b92e1a6d50f39285d54ddb464e2c0b29e3a65
alejandroCarreno <alejandro.carreno@datacivica.org>
2018-08-07 08:36:37 -0500

    explain bitwise operators in comments

--- 3-operators/44-bitwise_operators/main.cpp
+++ 3-operators/44-bitwise_operators/main.cpp
@@ -2,14 +2,14 @@
 
 int main(){
     unsigned int x = 4;
-    //4     = 0000 0100
-    //4<<1  = 0000 1000 = 8
+    //4     = 00000000 00000000 00000000 00000100
+    //4<<1  = 00000000 00000000 00000000 00001000 = 8
     std::cout << (x<<1) << std::endl;
 
     std::cout << "int uses " << sizeof(int) << " bytes" << "\n";
 
-    unsigned int y = 4;
-    std::cout << (~y) << std::endl;
+    // ~8   = 11111111 11111111 11111111 11110111 = 4294967291
+    std::cout << (~x) << std::endl;
 
     return 0;
 }
