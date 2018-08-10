cdd6313438592b328f75169d51b0df827a08ac3c
alejandroCarreno <alejandro.carreno@datacivica.org>
2018-08-10 10:07:58 -0500

    bit masks

---
+++ 3-operators/48-bit_masks/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bitset>
+
+int main(){
+  const unsigned int lowMask = 0xF;   // bit mask to keep low 4 bits(hex for 0000 0000 0000 1111)
+  int input; 
+
+  std::cout << "Enter integer: ";
+  std::cin >> input;
+  std::bitset<16> bitNum(input);
+
+  std::cout << "Number:\t" << input << " =\t" << bitNum << "\n\n";
+  input &= lowMask;
+
+  std::cout << "The 4 low bits have value =\t" << std::bitset<16>(input) << " = " << input << "\t"; 
+
+  return 0;
+}
