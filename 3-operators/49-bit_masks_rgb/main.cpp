bc8b465c0639eb35213040e9cf57229abe2288e5
mitsiu <mitsiu.carreno@gmail.com>
2018-08-13 00:41:35 -0500

    weird interpretation on hex value

--- 3-operators/49-bit_masks_rgb/main.cpp
+++ 3-operators/49-bit_masks_rgb/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <bitset>
 
 int main(){
   const unsigned int redBits   = 0xFF000000;
@@ -8,12 +9,17 @@ int main(){
 
   int pixel;
 
-  std::cout << "Ingresa un color de 32-bits en formato hexadecimal: ";
+  std::cout << "Ingresa un color de 32-bits en formato hexadecimal (FF7F3300): ";
   std::cin >> std::hex >> pixel;
 
- //Bitwise AND to isolate each color bits, then right shift the value into the 0-255
- unsigned char red = (pixel & redBits) >> 24;
- unsigned char green = (pixel & greenBits) >> 16;
+  std::cout << "Tu color es: " << pixel << "\n"; 
+  std::cout << "Tu color en binario es: " << std::bitset<32>(pixel) << "\n";
+
+  //Bitwise AND to isolate each color bits, then right shift the value into the 0-255
+  unsigned char red = (pixel & redBits) >> 24;
+  unsigned char green = (pixel & greenBits) >> 16;
+
+
 
   return 0;
 }
