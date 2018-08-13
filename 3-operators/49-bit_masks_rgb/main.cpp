e3ec37653fd8dba1420ae1dafd3c6227b24bb35f
mitsiu-carreno <mitsiu.carreno@gmail.com>
2018-08-13 12:42:30 -0500

    almost finish rgba excercise, problem on casting to int

--- 3-operators/49-bit_masks_rgb/main.cpp
+++ 3-operators/49-bit_masks_rgb/main.cpp
@@ -7,19 +7,26 @@ int main(){
   const unsigned int blueBits  = 0x0000FF00;
   const unsigned int alphaBits = 0x000000FF;
 
-  int pixel;
+ unsigned int pixel;
 
   std::cout << "Ingresa un color de 32-bits en formato hexadecimal (FF7F3300): ";
   std::cin >> std::hex >> pixel;
 
-  std::cout << "Tu color es: " << pixel << "\n"; 
+  std::cout << "Tu color es: " << std::hex << pixel  << "\n"; 
+  std::cout << "En número decimal es: " << pixel << "\n";
   std::cout << "Tu color en binario es: " << std::bitset<32>(pixel) << "\n";
 
   //Bitwise AND to isolate each color bits, then right shift the value into the 0-255
-  unsigned char red = (pixel & redBits) >> 24;
+  unsigned char red =   (pixel & redBits) >> 24;
   unsigned char green = (pixel & greenBits) >> 16;
+  unsigned char blue =  (pixel & blueBits) >> 8;
+  unsigned char alpha = (pixel & alphaBits);
 
-
+  std::cout << "Tu color contiene: " << "\n";
+  std::cout << static_cast<int> (red) << " red\n"; 
+  std::cout << static_cast<int> (green) << " green\n";
+  std::cout << static_cast<int> (blue) << " blue\n";
+  std::cout << static_cast<int> (alpha) << " alpha\n";
 
   return 0;
 }
