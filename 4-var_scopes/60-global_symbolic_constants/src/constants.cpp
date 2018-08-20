a5cdfb9a62daf5191e4281a25a93d450a8b6f6cc
mitsiu <mitsiu.carreno@gmail.com>
2018-08-19 21:55:01 -0500

    headers as extern variables

---
+++ 4-var_scopes/60-global_symbolic_constants/src/constants.cpp
@@ -0,0 +1,5 @@
+namespace Constants{
+	extern const double pi(3.141592);
+	extern const double avogadro(6.0221413e23);
+	extern const double my_gravity(9.81);
+}
---
+++ 4-var_scopes/60-global_symbolic_constants/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "constants.h"
+
+int main(){
+	
+	std::cout << "Constant pi have value:\t\t" << Constants::pi << "\n";
+	std::cout << "Constant avogadro have value:\t" << Constants::avogadro << "\n";
+	std::cout << "Constant my_gravity have value:\t" << Constants::my_gravity << "\n";
+
+	return 0;
+}
