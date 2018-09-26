bf28f08e0dab49c54f159026fefde747ad270929
mitsiu <mitsiu.carreno@gmail.com>
2018-09-26 01:37:40 -0500

    try finish switch init decl unsuccessfull

--- 5-flow_control/104-switch_var_declaration_initialization/main.cpp
+++ 5-flow_control/104-switch_var_declaration_initialization/main.cpp
@@ -5,18 +5,27 @@ int main(){
   // You can declare BUT NOT INITIALIZE variables inside the switch, both before and after the case laberls
 
   switch(1){
-    int a;  //OKAY, declaration is allowed before the case labels
-    int b = 5;    //ILLEGAL, initialization is not allowed before the case labels
+    int a;  					//OKAY, declaration is allowed before the case labels
+    // int b = 5;    	//ILLEGAL, initialization is not allowed before the case labels
 
     case 1:
-      int y;      // OK, declaration is allowed within a case
-      y = 4;      // OK, this is an assignment
+      int y;      		// OK, declaration is allowed within a case
+      y = 4;      		// OK, this is an assignment
       break;
 
     case 2:
-      y = 5;      // OK, y was declared above, so we can use it here too
+      y = 5;      		// OK, y was declared above, so we can use it here too
+			// Note that although variable y is defined in case 1, it is possible to use because statements under each case is not inside a implicit block TODO HERE!!!!
       break;
-  }
+
+		case 3:
+			//int z = 4;		//ILLEGAL, initialization is not allowed within a case
+			break;
+
+		default:
+			std::cout << "Default case\n";
+			break;
+	}
 
   return 0;
 }
