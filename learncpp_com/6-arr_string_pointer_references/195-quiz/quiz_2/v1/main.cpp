26836d9872cfd627de7c8ee21d1781aadfd9d16d
mitsiu <mitsiu.carreno@gmail.com>
2020-01-25 07:43:32 -0600

    start quiz 2, problem on passing vector to function

---
+++ learncpp_com/6-arr_string_pointer_references/195-quiz/quiz_2/v1/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+struct Student{
+  std::string name;
+  int grade;
+};
+
+bool validInput(){
+  if(std::cin.fail()){
+    std::cin.clear();
+    std::cin.ignore(32767, '\n');
+    return false;
+  }
+  std::cin.ignore(32767, '\n');
+  return true;
+}
+
+int getNumStudents(){
+  int input;
+
+  while(true){
+    std::cout << "Enter the number of students: ";
+    std::cin >> input;
+    if(validInput()){
+      return input;
+    }else{
+      std::cout << "Number not allowed, try again\n\n";
+    }
+  }
+}
+
+void fillStudents(std::vector studentRegistry){
+
+}
+
+int main(){
+  //Write the following program: Create a struct that holds a student’s first name and grade 
+  //(on a scale of 0-100). 
+  //Ask the user how many students they want to enter. 
+  //Create a std::vector to hold all of the students. 
+  //Then prompt the user for each name and grade. 
+  //Once the user has entered all the names and grade pairs, sort the list by grade 
+  //(highest first). Then print all the names and grades in sorted order.
+
+  int numStudents;
+
+  numStudents = getNumStudents();
+
+  std::vector<Student> studentRegistry;
+
+  fillStudents(studentRegistry);  
+
+  return 0;
+}
