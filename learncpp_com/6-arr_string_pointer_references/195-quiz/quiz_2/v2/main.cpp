4fe5051b12856a02b93c3d9c14faf68a18e16275
mitsiu <mitsiu.carreno@gmail.com>
2020-01-26 15:52:28 -0600

    finish quiz 2

--- learncpp_com/6-arr_string_pointer_references/195-quiz/quiz_2/v2/main.cpp
+++ learncpp_com/6-arr_string_pointer_references/195-quiz/quiz_2/v2/main.cpp
@@ -8,7 +8,112 @@
 //Once the user has entered all the names and grade pairs, sort the list by grade (highest first). 
 //Then print all the names and grades in sorted order.
 
+struct Student{
+  std::string name;
+  int grade;
+};
+
+bool isValidInput(){
+  if(std::cin.fail()){
+    std::cin.clear();
+    std::cin.ignore(32767, '\n');
+    return false;
+  }
+  std::cin.ignore(32767, '\n');
+  return true;
+}
+
+int getNumStudents(){
+  int input;
+  while(true){
+    std::cout << "How many students are you going to register?: ";
+    std::cin >> input;
+    if(isValidInput()){
+      return input;
+    }else{
+      std::cout << "Sorry that number seems weird lets try again\n\n";
+    }
+  }
+}
+
+std::string getStudentName(){
+  std::string input;
+  while(true){
+    std::cout << "Name?: ";
+    std::cin >> input;
+    if(isValidInput()){
+      return input;
+    }else{
+      std::cout << "That name seems weird let's try again\n\n";
+    }
+  }
+}
+
+int getStudentGrade(){
+  int input;
+  while(true){
+    std::cout << "Grade? (0-100): ";
+    std::cin >> input;
+    if(isValidInput()){
+      return input;
+    }else{
+      std::cout << "That grade seems weird let's tray again\n\n";
+    }
+  }
+}
+
+void fillStudents(Student *studentRegistry, int studentRegistryLength){
+  std::cout << "Let's fill each student info: \n";
+  for(int i{0}; i < studentRegistryLength; ++i){
+    std::cout << "Student " << i+1 << ":" << std::endl;
+
+    studentRegistry[i].name = getStudentName();
+
+    studentRegistry[i].grade = getStudentGrade();
+    std::cout << std::endl;
+  } 
+  std::cout << std::endl; 
+}
+
+void sortByGrade(Student *studentRegistry, int studentRegistryLength){
+  for(int superi {0}; superi < studentRegistryLength; ++superi){
+    int bubbleCurrentMax {0};
+    bool earlyStop {true};
+    for(int i {studentRegistryLength-1}; i >=0; --i){
+      if(bubbleCurrentMax < studentRegistry[i].grade){
+        bubbleCurrentMax = studentRegistry[i].grade;
+      }else{
+        earlyStop = false;
+        Student temp {studentRegistry[i]};
+        studentRegistry[i] = studentRegistry[i+1];
+        studentRegistry[i+1] = temp;
+      }
+    }
+    if(earlyStop && (superi != studentRegistryLength -1)){
+      std::cout << "Early stoping the bubble sort algorithm was detected and implemented, please double check sorting\n";
+      return;
+    }
+  }  
+}
+
+void printStudentRegistry(Student *studentRegistry, int studentRegistryLength){
+  for(int i {0}; i < studentRegistryLength; ++i){
+    std::cout << studentRegistry[i].name << " got a grade of " << studentRegistry[i].grade << std::endl;
+  }
+}
 
 int main(){
+  int numOfStudents = getNumStudents();
+
+  Student *studentRegistry = new Student[numOfStudents];
+
+  fillStudents(studentRegistry, numOfStudents); 
+
+  sortByGrade(studentRegistry, numOfStudents);
+
+  printStudentRegistry(studentRegistry, numOfStudents);
+
+  delete[] studentRegistry;
+
   return 0;
 }
