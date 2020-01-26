e6d94f50c61e6d047e63308faa0309f5f2f1c083
mitsiu <mitsiu.carreno@gmail.com>
2020-01-26 17:23:50 -0600

    quiz 3

--- learncpp_com/6-arr_string_pointer_references/195-quiz/quiz_3/main.cpp
+++ learncpp_com/6-arr_string_pointer_references/195-quiz/quiz_3/main.cpp
@@ -1,3 +1,25 @@
+#include <iostream>
+
 //Write your own function to swap the value of two integer variables. 
 //Write a main() function to test it.
 //Hint: Use reference parameters
+
+void swap(int &a, int &b){
+  int temp = a;
+  a = b;
+  b = temp;
+}
+
+int main(){
+
+  int a = 10;
+  int b = 80;
+
+  std::cout << "a: " << a << " b: " << b << std::endl;
+  
+  swap(a, b);
+
+  std::cout << "a: " << a << " b: " << b << std::endl;
+  
+  return 0;
+}
