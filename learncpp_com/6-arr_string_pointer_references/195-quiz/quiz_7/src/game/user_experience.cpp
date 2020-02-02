d073550587cbd47eb75ded35b647dbed60f7b628
mitsiu <mitsiu.carreno@gmail.com>
2020-02-01 18:15:00 -0600

    finish card printer (UX)

--- learncpp_com/6-arr_string_pointer_references/195-quiz/quiz_7/src/game/user_experience.cpp
+++ learncpp_com/6-arr_string_pointer_references/195-quiz/quiz_7/src/game/user_experience.cpp
@@ -4,12 +4,50 @@
 #include "card.hpp"
 
 namespace userexperience {
-  void DrawCards(const std::vector<card::Card> &deck){
+
+  void DrawTopBottomCards(const std::vector<card::Card> &deck){
     
     for(const card::Card &card : deck){
-      std::cout << "*" << card::GetCardRankSuit(card) << "   *";
+      std::string card_rank_suit = card::GetCardRankSuit(card);
+      std::string card_top {"*****"};
+      for(int i {0}; i < static_cast<int>(card_rank_suit.length()); ++i){
+        card_top += "*";
+      }
+      std::cout << card_top << "\t";
+    }
+    
+    std::cout << std::endl;
+
+  }
+
+  void DrawCardSides(const std::vector<card::Card> &deck){
+        
+    for(const card::Card &card : deck){
+      std::string card_rank_suit = card::GetCardRankSuit(card);
+      std::string card_border {"*   "};
+      for(int i {0}; i < static_cast<int>(card_rank_suit.length()); ++i){
+        card_border += " ";
+      }
+      card_border += "*";
+      std::cout << card_border << "\t";
+    }
+    
+    std::cout << std::endl;
+  }
+
+  void DrawCards(const std::vector<card::Card> &deck){
+    
+    DrawTopBottomCards(deck);
+
+    for(const card::Card &card : deck){
+      std::cout << "*" << card::GetCardRankSuit(card) << "   *\t";
       
     }
-    std::cout << "to display " << deck.size() << " cards\n";
+
+    std::cout << std::endl;
+
+    DrawCardSides(deck);
+    DrawCardSides(deck);
+    DrawTopBottomCards(deck);
   }  
 }
